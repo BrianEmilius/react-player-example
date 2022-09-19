2f01ce5025d271b4b7b6be0d64b11fa3bda8d0d3
Brian Emilius <be@rts.dk>
2022-09-19 12:46:10 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import AudioPlayer from "react-h5-audio-player"
+import "react-h5-audio-player/lib/styles.css";
+import { useEffect, useState } from 'react';
+
 
 function App() {
+  const [trackIndex, setTrackIndex] = useState(false)
+  const musicTracks = [
+    {
+      name: "Cantina Band",
+      src: "https://github.com/cookieman2002/iplay-music-mp3/blob/main/Cantina%20Band.mp3?raw=true"
+    },
+    {
+      name: "Memories",
+      src: "https://github.com/cookieman2002/iplay-music-mp3/blob/main/bensound-memories.mp3?raw=true"
+    },
+  ]
+  
+  useEffect(function() {
+    setTrackIndex(0)
+  }, [])
+
+  const handleClickPrevious = () => {
+    setTrackIndex((currentTrack) =>
+      currentTrack === 0 ? musicTracks.length - 1 : currentTrack - 1
+    )
+  }
+
+  const handleClickNext = () => {
+    setTrackIndex((currentTrack) =>
+      currentTrack < musicTracks.length - 1 ? currentTrack + 1 : 0
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {trackIndex !== false && (<AudioPlayer
+        autoPlay
+        src={musicTracks[trackIndex].src}
+        onPlay={e => console.log("onPlay")}
+        header={musicTracks[trackIndex].name}
+        onClickPrevious={handleClickPrevious}
+        onClickNext={handleClickNext}
+        onEnded={handleClickNext}
+      />)}
     </div>
   );
 }
